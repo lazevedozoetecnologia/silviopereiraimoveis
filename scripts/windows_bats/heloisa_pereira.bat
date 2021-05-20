969f603d3af72e3485366ff134d3c73b0f94cd8f
Lucas Azevedo <lazevedo.lucas@gmail.com>
2021-05-20 16:52:30 -0300

    Added bat for Heloisa Pereira

---
+++ scripts/windows_bats/heloisa_pereira.bat
@@ -0,0 +1,8 @@
+net use * /d /y
+
+net use P: \\srvfs01\publico /user:heloisa.pereira 6hgT87U6 /persistent:yes
+net use T: \\srvfs01\temporario /user:heloisa.pereira 6hgT87U6 /persistent:yes
+net use Z: \\srvfs01\comercial /user:heloisa.pereira 6hgT87U6 /persistent:yes
+net use Y: \\srvfs01\administrativo /user:heloisa.pereira 6hgT87U6 /persistent:yes
+net use L: \\srvfs01\locacao /user:heloisa.pereira 6hgT87U6 /persistent:yes
+net use W: \\srvfs01\carteira /user:heloisa.pereira 6hgT87U6 /persistent:yes
